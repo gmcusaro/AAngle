a3225cfc93bb21cc5b9292f894db6bd17ff02e7b
Giovanni Maria Cusaro <gcusaro@me.com>
2025-02-05 20:27:12 +0100

    Set platforms

--- Package.swift
+++ Package.swift
@@ -4,14 +4,23 @@ import PackageDescription
 
 let package = Package(
     name: "AAngle",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+        .visionOS(.v2),
+        .watchOS(.v10),
+        .tvOS(.v17)
+    ],
     products: [
         .library(
             name: "AAngle",
-            targets: ["AAngle"]),
+            targets: ["AAngle"]
+        ),
     ],
     targets: [
         .target(
-            name: "AAngle"),
+            name: "AAngle"
+        ),
         .testTarget(
             name: "AAngleTests",
             dependencies: ["AAngle"]
