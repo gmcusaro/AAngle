45ab992ad442f9b7b95f6587611da6b8527ec184
Giovanni Maria Cusaro <gcusaro@me.com>
2025-02-09 10:31:23 +0100

    Add precondition, @inlinable, trigonometry `oppostie()` and `adjacentAngles()`

--- Sources/AAngle/Units/Degrees.swift
+++ Sources/AAngle/Units/Degrees.swift
@@ -5,7 +5,10 @@ public struct Degrees: Hashable, Codable, Sendable, Anglable {
     
     /// Initializes a `Degrees` instance with a raw `Double` value.
     /// - Parameter rawValue: The angle in degrees as a `Double`.
+    @inlinable
     public init(_ rawValue: Double) {
+        // Ensure the rawValue is finite (not NaN or infinite)
+        precondition(rawValue.isFinite, "Degrees must be initialized with a finite value.")
         self.rawValue = rawValue
     }
     
@@ -16,7 +19,30 @@ public struct Degrees: Hashable, Codable, Sendable, Anglable {
     /// This allows the `Degrees` value to be used with `Measurement`-based APIs, such as those that require units of angle.
     ///
     /// - Returns: A `Measurement<UnitAngle>` representing the angle in degrees.
+    @inlinable
     public func toMeasurement() -> Measurement<UnitAngle> {
         return Measurement<UnitAngle>(value: rawValue, unit: .degrees)
+    }
+}
+
+extension Degrees {
+    /// Compute the opposite angle (adds 180°)
+    @inlinable
+    public var opposite: Degrees {
+         var oppositeAngle = Degrees(rawValue + Self.normalizationValue / 2)
+         oppositeAngle.normalize()
+         return oppositeAngle
+     }
+
+    /// Compute the adjacent angles (90° and 270° away)
+    @inlinable
+    public func adjacentAngles() -> [Degrees] {
+        let adj1 = (rawValue + 90).truncatingRemainder(dividingBy: Self.normalizationValue)
+        let adj2 = (rawValue - 90).truncatingRemainder(dividingBy: Self.normalizationValue)
+        
+        let normalizedAdj1 = adj1 < 0 ? adj1 + Self.normalizationValue : adj1
+        let normalizedAdj2 = adj2 < 0 ? adj2 + Self.normalizationValue : adj2
+        
+        return [Degrees(normalizedAdj1), Degrees(normalizedAdj2)]
     }
 }
