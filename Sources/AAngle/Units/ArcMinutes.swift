ecde87dd17b355aa5eb72f15a0be7b2b4bf6eb1a
Giovanni Maria Cusaro <gcusaro@me.com>
2025-02-12 12:22:14 +0100

    Fix indent

--- Sources/AAngle/Units/ArcMinutes.swift
+++ Sources/AAngle/Units/ArcMinutes.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public struct ArcMinutes: Hashable, Codable, Sendable, Anglable {
     public var rawValue: Double
-    
+
     /// Initializes a `ArcMinutes` instance with a raw `Double` value.
-    /// 
+    ///
     /// - Parameter rawValue: The angle in arc minute as a `Double`.
     @inlinable
     public init(_ rawValue: Double) {
@@ -12,7 +12,7 @@ public struct ArcMinutes: Hashable, Codable, Sendable, Anglable {
         precondition(rawValue.isFinite, "Arc Minutes must be initialized with a finite value.")
         self.rawValue = rawValue
     }
-    
+
     /// The normalization value used for converting and normalizing arc minutes.
     public static let normalizationValue: Double = 21_600
 
