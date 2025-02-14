3a70050d8e3d5a2b005784bd834d0f0280a7f42d
Giovanni Maria Cusaro <gcusaro@me.com>
2025-02-14 09:09:18 +0100

    Add missing documentation and `initAngle(_ value: any Anglable)`

--- Sources/AAngle/AngleType.swift
+++ Sources/AAngle/AngleType.swift
@@ -5,6 +5,7 @@ public enum AngleType: String, Codable, Equatable, Hashable {
 }
 
 public extension AngleType {
+    /// Human-readable description of the angle type.
     var description: String {
         switch self {
         case .gradians:    return "gradians"
@@ -18,15 +19,48 @@ public extension AngleType {
 }
 
 public extension AngleType {
+    /// Initializes an angle instance using a floating-point value.
+    ///
+    /// - Parameter value: The value of the angle in the specified `AngleType` unit.
+    /// - Returns: An `Anglable` instance representing the angle.
     func initAngle<T: BinaryFloatingPoint>(_ value: T) -> any Anglable {
         self.initAngle(Double(value))
     }
-
+    
+    /// Initializes an angle instance using an integer value.
+    ///
+    /// - Parameter value: The value of the angle in the specified `AngleType` unit.
+    /// - Returns: An `Anglable` instance representing the angle.
     func initAngle<T: BinaryInteger>(_ value: T) -> any Anglable {
         self.initAngle(Double(value))
     }
-
+    
+    /// Initializes an angle instance using a `Double` value.
+    ///
+    /// - Parameter value: The value of the angle in the specified `AngleType` unit.
+    /// - Returns: An `Anglable` instance representing the angle.
     func initAngle(_ value: Double) -> any Anglable {
+        switch self {
+        case .degrees:
+            return Degrees(value)
+        case .radians:
+            return Radians(value)
+        case .gradians:
+            return Gradians(value)
+        case .revolutions:
+            return Revolutions(value)
+        case .arcMinutes:
+            return ArcMinutes(value)
+        case .arcSeconds:
+            return ArcSeconds(value)
+        }
+    }
+    
+    /// Converts an existing `Anglable` instance to the specified `AngleType`.
+    ///
+    /// - Parameter value: An existing `Anglable` instance to be converted.
+    /// - Returns: A new `Anglable` instance in the specified `AngleType` unit.
+    func initAngle(_ value: any Anglable) -> any Anglable {
         switch self {
         case .degrees:
             return Degrees(value)
