30aec2d85ef5e4c619ea1f1f86990db1c51ece02
Giovanni Maria Cusaro <gcusaro@me.com>
2025-03-23 13:50:12 +0100

    Complete tests

--- Tests/AAngleTests/AngleTypeTests.swift
+++ Tests/AAngleTests/AngleTypeTests.swift
@@ -4,14 +4,14 @@ import Testing
 @Test func testAngleType() throws {
     let radiansType: Degrees = AngleType.degrees.initAngle(180) as! Degrees
     let value: Revolutions = radiansType.convertTo(.revolutions) as! Revolutions
-    print(value.rawValue)
+    #expect(value.rawValue == 0.5)
     
     let angleInRadians = Radians(.pi / 4)
     let oppositeAngleInRadians = angleInRadians
-    print(Degrees(oppositeAngleInRadians))
+    #expect(Degrees(oppositeAngleInRadians).rawValue == 45.0)
     
     var myRev = Revolutions()
     myRev += 0.5
     myRev += Degrees(180)
-    print(myRev.rawValue)
+    #expect(myRev.rawValue == 0.0)
 }
